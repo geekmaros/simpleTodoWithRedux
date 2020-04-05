ac431a637c1c5a5802952c3769fb7d2c8314f6c2
geekmaros <razicruz@gmail.com>
2020-04-05 19:34:10 +0100

    added mobile responisveness and solved input bug

--- src/components/search-bar/search-bar.components.jsx
+++ src/components/search-bar/search-bar.components.jsx
@@ -1,56 +1,57 @@
-import React,{Component} from 'react'
-import './search-bar.styles.sass'
-import {connect} from 'react-redux'
+import React, { Component } from "react";
+import "./search-bar.styles.sass";
+import { connect } from "react-redux";
 import * as TodoActions from "../../redux/todo.action";
-import {bindActionCreators} from "redux";
+import { bindActionCreators } from "redux";
 
-class SearchBarComponents extends Component{
+class SearchBarComponents extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userInput: "",
+    };
+  }
 
-    constructor() {
-        super();
-        this.state = {
-            userInput: ''
-        }
-
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.userInput === "") {
+      alert("Invalid Input");
+      return;
     }
+    // console.log('working')
+    // this.setState({userInput: ''})
+    this.props.actions.addTodo(this.state.userInput);
 
-    handleSubmit = e => {
-        e.preventDefault()
-        // if (this.state.userInput === '') {
-        //     alert('Invalid Input')
-        //     return
-        // }
-        // console.log('working')
-        // this.setState({userInput: ''})
-        this.props.actions.addTodo(this.state.userInput)
-        this.setState({userInput: ''})
+    this.setState({ userInput: "" }, function () {
+      console.log(this.state.userInput);
+    });
+  };
 
+  handleChange = (e) => {
+    e.preventDefault();
+    const { value } = e.target;
+    this.setState({ userInput: value });
+  };
 
-
-
-    }
-
-
-    handleChange = e => {
-        e.preventDefault()
-        const {value} = e.target
-        this.setState({userInput: value})
-
-
-    }
-    render() {
-        return (
-            <div className='searchWrapper'>
-                <form onSubmit={this.handleSubmit}>
-                    <input type="text" onChange={this.handleChange}  className='input'  placeholder="Enter Todo "/>
-                </form>
-            </div>
-        );
-    }
-
+  render() {
+    return (
+      <div className="searchWrapper">
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            onChange={this.handleChange}
+            value={this.state.userInput}
+            className="input"
+            placeholder="Enter Todo "
+          />
+        </form>
+      </div>
+    );
+  }
 }
-const mapDispatchToProps = dispatch => ({
+
+const mapDispatchToProps = (dispatch) => ({
   // addTodo: todo => dispatch(addTodo(todo))
-    actions: bindActionCreators(TodoActions, dispatch)
-})
-export default connect(null, mapDispatchToProps)(SearchBarComponents)
+  actions: bindActionCreators(TodoActions, dispatch),
+});
+export default connect(null, mapDispatchToProps)(SearchBarComponents);
