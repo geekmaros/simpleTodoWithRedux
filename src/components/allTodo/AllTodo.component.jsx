0869e6642de880f90dd178aff02235f5a6fb6610
geekmaros <razicruz@gmail.com>
2020-04-15 21:09:19 +0100

    added todo header

--- src/components/allTodo/AllTodo.component.jsx
+++ src/components/allTodo/AllTodo.component.jsx
@@ -6,6 +6,7 @@ import './allTodo.style.sass'
 const AllTodoComponent = ({todos}) => {
     return (
         <div className='container-fluid'>
+            <h1 className='title' >TODOS++</h1>
 
             {
                 todos.map((todo, i) => <SingleTodo key={i} todo={todo}/> )
